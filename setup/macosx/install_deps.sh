52ef6469510cf352e43450a989467df61ffb1dd5
Sam Townsend <stownsend42@sbcglobal.net>
2020-11-26 10:56:02 -0600

    Updated Lazarus to 2.0.10, FPC to 3.2.0 for macOS

--- setup/macosx/install_deps.sh
+++ setup/macosx/install_deps.sh
@@ -3,19 +3,21 @@
 set -x
 set -e
 
-lazarus_ver="2.0.8"
-fpc="fpc-3.0.4-macos-x86_64-laz"
-lazarus="LazarusIDE-2.0.8-macos-x86_64"
+lazarus_ver="2.0.10"
+fpc="fpc-3.2.0.intel-macosx"
+fpc_package="fpc-3.2.0-intel-macosx"
+lazarus="Lazarus-2.0.10-x86_64-macosx"
 
 if [ -n "${sourceforge_mirror-}" ]; then
   mirror_string="&use_mirror=${sourceforge_mirror}"
 fi
 
 if [ ! -x "$(command -v fpc 2>&1)" ]; then
-  wget "https://downloads.sourceforge.net/project/lazarus/Lazarus%20macOS%20x86-64/Lazarus%20${lazarus_ver}/$fpc.pkg?r=&ts=$(date +%s)${mirror_string-}" -O "fpc.pkg"
-  sudo ln -s /usr/local/lib/fpc/3.0.4/ppcx64 /usr/local/bin/ppcx64
-  sudo installer -pkg "fpc.pkg" -target /
-  rm "fpc.pkg"
+  wget "https://downloads.sourceforge.net/project/lazarus/Lazarus%20macOS%20x86-64/Lazarus%20${lazarus_ver}/$fpc.dmg?r=&ts=$(date +%s)${mirror_string-}" -O "fpc.dmg"
+  hdiutil attach fpc.dmg
+  sudo installer -pkg "/Volumes/$fpc/$fpc_package.pkg" -target /
+  hdiutil detach "/Volumes/$fpc"
+  rm "fpc.dmg"
 fi
 
 if [ ! -x "$(command -v lazbuild 2>&1)" ]; then
